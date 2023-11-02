c2b126cc11245e07a9c4a06aa4be8cf2d9532eaf
temple7447 <starukido@gmail.com>
2023-11-02 19:30:31 +0100

    df

--- Model/Product.js
+++ Model/Product.js
@@ -15,7 +15,7 @@ const databaseSchema = new Schema({
     productStatus: { type: String, required: true },
     productStatusState: { type: Boolean, required: true },
     longD: { type: String, required: true },
-    Specificationobject: { type:  { type: String, required: true}},
+    Specificationobject: { type: Schema.Types.Mixed, required: true},
     Title: { type: String, required: true },
     Price: { type: Number, required: true },
     totalQuantity: { type: Number, required: true },
